c8ea36fdf60ee7bad0a039c626d4363c5ff338e7
Craig Johnston <cjimti@gmail.com>
2018-04-21 09:59:23 -0700

    added content-type to CORS headers

--- rxtx.go
+++ rxtx.go
@@ -82,6 +82,6 @@ func main() {
 // CORS
 func preflight(c *gin.Context) {
 	c.Header("Access-Control-Allow-Origin", "*")
-	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers, content-type")
 	c.JSON(http.StatusOK, struct{}{})
 }
